063f5a85db02f164da60a814f9518b6c7f318f99
AdibPN <apaulinineto@gmail.com>
2022-10-12 21:27:32 -0300

    séries finalizadas

--- Hamburgao/Hamburgao/Views/ComponentesSegundo.xaml.cs
+++ Hamburgao/Hamburgao/Views/ComponentesSegundo.xaml.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using Hamburgao.Models;
+
 namespace Hamburgao.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -15,6 +17,127 @@ namespace Hamburgao.Views
         public ComponentesSegundo()
         {
             InitializeComponent();
+        }
+
+            private async void Button_Clicked(object sender, EventArgs e)
+            {
+                try
+                {
+                    var c = new Componentes
+                    {
+                        Nome = "Ética",
+                        AtribuicoesResponsabilidades = "Habilitação em Gestão de Negócios, Direito, Gestão de Políticas Públicas, etc.",
+                        ValoresAtitudes = "Desenvolver o olhar ético, responsabilizar-se pelas atitudes, gestão de pessoas.",
+                    };
+
+                    await Navigation.PushAsync(new VerComponente(c));
+
+                }
+                catch (Exception ex)
+                {
+                    await DisplayAlert("IXI", ex.Message, "OK");
+                }
+
+            }
+
+        private async void Button_Clicked1(object sender, EventArgs e)
+        {
+            try
+            {
+                var c = new Componentes
+                {
+                    Nome = "Desenvolvimento de Sistemas",
+                    AtribuicoesResponsabilidades = "Engenharia de Sistemas, Engenharia de Software, Matemática Aplicada, Computação Científica etc.",
+                    ValoresAtitudes = "Desenvolver habilidades de Informática com Ênfase em Banco de Dados, Redes de Computadores, e outras linguagens, como, C#.",
+                };
+
+                await Navigation.PushAsync(new VerComponente(c));
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("IXI", ex.Message, "OK");
+            }
+        }
+
+        private async void Button_Clicked2(object sender, EventArgs e)
+        {
+            try
+            {
+                var c = new Componentes
+                {
+                    Nome = "Banco de dados II",
+                    AtribuicoesResponsabilidades = "Utilização de ambiente de desenvolvimento integrado para a execução de instruções de bancos de dados.",
+                    ValoresAtitudes = "Desenvolver a Análise de Sistemas Administrativos em Processamento de Dados.",
+                };
+
+                await Navigation.PushAsync(new VerComponente(c));
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("IXI", ex.Message, "OK");
+            }
+        }
+
+        private async void Button_Clicked3(object sender, EventArgs e)
+        {
+            try
+            {
+                var c = new Componentes
+                {
+                    Nome = "Programação Mobile",
+                    AtribuicoesResponsabilidades = "Tecnologia em Desenvolvimento de Jogos Digitais, Tecnologia em Desenvolvimento de Software",
+                    ValoresAtitudes = "Desenvolver aplicativos que utilizem todas as matérias como agragação",
+                };
+
+                await Navigation.PushAsync(new VerComponente(c));
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("IXI", ex.Message, "OK");
+            }
+        }
+
+        private async void Button_Clicked4(object sender, EventArgs e)
+        {
+            try
+            {
+                var c = new Componentes
+                {
+                    Nome = "Sistemas Embarcados",
+                    AtribuicoesResponsabilidades = "Utilização de ambiente de desenvolvimento integrado para a construção de instruções para sistemas embarcados.",
+                    ValoresAtitudes = "Desenvolver programas integradas à sistemas embarcados que sejam úteis no cotidiano",
+                };
+
+                await Navigation.PushAsync(new VerComponente(c));
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("IXI", ex.Message, "OK");
+            }
+        }
+
+        private async void Button_Clicked5(object sender, EventArgs e)
+        {
+            try
+            {
+                var c = new Componentes
+                {
+                    Nome = "Programação Web II",
+                    AtribuicoesResponsabilidades = "Utilização de ambiente de desenvolvimento integrado para construção de páginas de Internet, linguagem de script e folhas de estilo.",
+                    ValoresAtitudes = "Desenvolver página na web, com raciocínio lógico e idéias de layout",
+                };
+
+                await Navigation.PushAsync(new VerComponente(c));
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("IXI", ex.Message, "OK");
+            }
         }
     }
 }
--- Hamburgao/Hamburgao/Views/ComponentesTerceiro.xaml.cs
+++ Hamburgao/Hamburgao/Views/ComponentesTerceiro.xaml.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using Hamburgao.Models;
+
 namespace Hamburgao.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -15,6 +17,134 @@ namespace Hamburgao.Views
         public ComponentesTerceiro()
         {
             InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var c = new Componentes
+                {
+                    Nome = "Segurança de Sistemas de Informações",
+                    AtribuicoesResponsabilidades = "Implementar rotinas de segurança da informação",
+                    ValoresAtitudes = "Estimular atitudes respeitosas, incentivar comportamentos éticos, desenvolver a criticidade",
+                };
+
+                await Navigation.PushAsync(new VerComponente(c));
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("IXI", ex.Message, "OK");
+            }
+
+        }
+
+        private async void Button_Clicked1(object sender, EventArgs e)
+        {
+            try
+            {
+                var c = new Componentes
+                {
+                    Nome = "Desenvolvimento do trabalho de conclusão de curso (TCC) em Desenvolvimento de Sistemas",
+                    AtribuicoesResponsabilidades = "Habilitação em Gestão de Negócios, Direito, Gestão de Políticas Públicas, etc.",
+                    ValoresAtitudes = "Desenvolver o olhar ético, responsabilizar-se pelas atitudes, gestão de pessoas.",
+                };
+
+                await Navigation.PushAsync(new VerComponente(c));
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("IXI", ex.Message, "OK");
+            }
+
+        }
+
+
+        private async void Button_Clicked2(object sender, EventArgs e)
+        {
+            try
+            {
+                var c = new Componentes
+                {
+                    Nome = "Banco de dados III",
+                    AtribuicoesResponsabilidades = "Realizar a gestão de banco de dados",
+                    ValoresAtitudes = "incentivar a criatividade, desenvolver a criticidade, fortalecer a persistencia e o interesse na resolução de problemas",
+                };
+
+                await Navigation.PushAsync(new VerComponente(c));
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("IXI", ex.Message, "OK");
+            }
+
+        }
+
+
+        private async void Button_Clicked3(object sender, EventArgs e)
+        {
+            try
+            {
+                var c = new Componentes
+                {
+                    Nome = "Programação Mobile II",
+                    AtribuicoesResponsabilidades = "documentar, construitr e manter  sistemas de informações em plataformas móveis",
+                    ValoresAtitudes = "Responsabilizar-se pela produção, utilização e divulgação de informações, estimular a organização, etc. ",
+                };
+
+                await Navigation.PushAsync(new VerComponente(c));
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("IXI", ex.Message, "OK");
+            }
+
+        }
+
+        private async void Button_Clicked4(object sender, EventArgs e)
+        {
+            try
+            {
+                var c = new Componentes
+                {
+                    Nome = "Qualidade e teste de sofware",
+                    AtribuicoesResponsabilidades = "testar sofwares para melhoria da qualidade de sistemas, elaborar registros e planilhas de acompanhamento e controle das atividades ",
+                    ValoresAtitudes = "responsabilizar-se pela produção, utilização e divulgação de informações, etc.",
+                };
+
+                await Navigation.PushAsync(new VerComponente(c));
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("IXI", ex.Message, "OK");
+            }
+
+        }
+
+        private async void Button_Clicked5(object sender, EventArgs e)
+        {
+            try
+            {
+                var c = new Componentes
+                {
+                    Nome = "Programação Web III",
+                    AtribuicoesResponsabilidades = "documentar, contruir e manter sistemas de informações para web",
+                    ValoresAtitudes = "responsabilizar-se pela produçaõ, utilização e divulgação de informações, fortalecer a persistencia e o interesse na resolução de problemas, incentivar a criatividade",
+                };
+
+                await Navigation.PushAsync(new VerComponente(c));
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("IXI", ex.Message, "OK");
+            }
+
         }
     }
 }
